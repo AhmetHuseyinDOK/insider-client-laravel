fa5c39bbac8c455d66f6338789e86d075efb70a6
ahmethuseeyindok <dokahmethuseyin@gmail.com>
2019-07-14 11:43:53 +0300

    added mock endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,21 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/product/{id}',function(){
+    return view('product',['id' => request()->id]);
+});
+
+Route::post('/coupon',function(){
+    
+    return [
+        "customer_id" => request()->customer_id,
+        "product_id" => request()->product_id,
+        "code"=> request()->customer_id."DIS20TL",
+        "direct"=>20,
+        "expires"=>"2019-07-14 23:00:00"
+    ];
 });
 
 Route::get('/user',function(){
